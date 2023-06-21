5091bba8cf47f1745ef613d66f7ccd43d2ad7025
gitkonio <gitkonio@kriptonio.com>
2023-06-21 10:03:57 +0200

    explicitly specify entity addresses

--- index.ts
+++ index.ts
@@ -9,13 +9,23 @@ async function main() {
   // Create a wallet instance from the private key
   const owner = new ethers.Wallet(privateKey);
 
+  // Entry point and factory addresses for Polygon Mumbai testnet
+  const entryPointAddress = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
+  const factoryAddress = '0x9406Cc6185a346906296840746125a0E44976454';
+
   const smartAccount = await Presets.Builder.SimpleAccount.init(
     owner,
     bundlerRpcUrl,
+    {
+      entryPoint: entryPointAddress,
+      factory: factoryAddress,
+    }
   );
   console.log('smart wallet address', smartAccount.getSender());
 
-  const client = await Client.init(bundlerRpcUrl);
+  const client = await Client.init(bundlerRpcUrl, {
+    entryPoint: entryPointAddress,
+  });
 
   const result = await client.sendUserOperation(
     smartAccount.execute(smartAccount.getSender(), 0, "0x"),
